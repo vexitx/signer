f8ae69e98c78c06f053a09f489c6de68180f0e3a
humbledneuron <blogger.thomasjerry@gmail.com>
2025-02-05 00:41:03 +0530

    fix: correct QR code handling function and improve image generation

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ import qrcode
 import base64
 from io import BytesIO
 
-import qrcode.constants
-
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
@@ -21,27 +19,27 @@ def swedbank():
 
 
 @socketio.on('qr_code_scanned')
-def hanlde_qr_code(data):
+def handle_qr_code(data):
     qr_data = data['data']
-    print(f"[Debug] Qr code received; {qr_data}")
-
+    print(f"[Debug] QR code received: {qr_data}")
+    
     qr = qrcode.QRCode(
         version=1,
-        error_correction=qrcode.constants.ERROR_CORRCT_L,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,  
         box_size=10,
         border=4,
     )
 
     qr.add_data(qr_data)
     qr.make(fit=True)
-    qr_img = qr.make_image(fill_color="neon", back_color="red")
+    qr_img = qr.make_image(fill_color="black", back_color="white")
 
     buffered = BytesIO()
     qr_img.save(buffered, format="PNG")
     img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-    socketio.emitaa("update_qr_code_image", {'qr_image': img_base64})
+    socketio.emit("update_qr_code_image", {'qr_image': img_base64})  
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000, debug="True")
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)  
