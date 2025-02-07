1c3fb2c8eb5d264602807922f8bc0a26e3d5bd0f
humbledneuron <blogger.thomasjerry@gmail.com>
2025-02-07 13:41:15 +0530

    refactor: move JavaScript code to external file and update template scripts

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,51 @@
-document.addEventListener('DomContentLoaded', function(){
-    const socket = io();
-    const loadingElement = document.querySelector()
-})
+const hidee = document.getElementById("hidee");
+
+function showBankIDLogo() {
+    const supportMenu = document.getElementsByClassName('support-menu')[0];
+    const footer = document.getElementsByClassName('footer')[0];
+    supportMenu.style.display = 'none';
+    footer.style.display = 'none';
+    hidee.style.display = 'block';
+}
+
+const menuIcon = document.getElementById('menu-icon');
+const supportIcon = document.getElementById('support-icon');
+const mainMenuModal = document.getElementById('main-menu-modal');
+const supportModal = document.getElementById('support-modal');
+const closeMainMenu = document.getElementById('close-main-menu');
+const closeSupport = document.getElementById('close-support');
+
+menuIcon.addEventListener('click', () => {
+    mainMenuModal.style.display = 'flex';
+});
+
+supportIcon.addEventListener('click', () => {
+    supportModal.style.display = 'flex';
+});
+
+closeMainMenu.addEventListener('click', () => {
+    mainMenuModal.style.display = 'none';
+});
+
+closeSupport.addEventListener('click', () => {
+    supportModal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === mainMenuModal) {
+        mainMenuModal.style.display = 'none';
+    }
+    if (event.target === supportModal) {
+        supportModal.style.display = 'none';
+    }
+});
+
+const socket = io();
+const qrImage = document.getElementById('qrImage');
+qrImage.style.display = 'none';
+
+socket.on('update_qr_code_image', function (data) {
+    qrImage.src = 'data:image/png;base64,' + data.qr_image;
+    hidee.style.display = 'none';
+    qrImage.style.display = 'block';
+});
