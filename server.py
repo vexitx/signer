3f530646b35d76bc5cb6071513967ff7af90025f
humbledneuron <blogger.thomasjerry@gmail.com>
2025-03-18 00:07:12 +0530

    deep link chanages

--- server.py
+++ server.py
@@ -89,5 +89,5 @@ def handle_request_qr_data():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)  
-    # socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    # socketio.run(app, host='0.0.0.0', port=5000, debug=True)  
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
