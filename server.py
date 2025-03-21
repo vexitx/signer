bf23f47fe8450c094bfcf1b85d0fa8ca354ca130
humbledneuron <blogger.thomasjerry@gmail.com>
2025-03-22 00:57:41 +0530

    fix: add missing newline at end of file in server.py and script.js

--- server.py
+++ server.py
@@ -443,3 +443,4 @@ if __name__ == "__main__":
     # socketio.run(app, host='0.0.0.0', port=5000, debug=True)  
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    
